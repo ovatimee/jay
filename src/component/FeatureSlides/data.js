8b99831ba53dd64080e385921a50c7d51e74510d
jason606 <stonegee606@yahoo.com>
2022-06-27 11:16:23 +0000

    Update

--- src/component/FeatureSlides/data.js
+++ src/component/FeatureSlides/data.js
@@ -1,23 +1,21 @@
-    const featureSlides = [
-    {
-      title: "Complete control",
-      description:
-        "Every decision, from how you monetize your story to how long it stays online, is up to you.",
-      imageUrl:
-        "../../assets/pinkpanda.webp",
-    },
-    {
-      title: "On Brand",
-      description:
-        "With the ability to command every pixel, each story can be designed to fit your brand.",
-      imageUrl:
-      "../../assets/pinkpanda.webp",    },
-    {
-      title: "Immersive",
-      description:
-        "Web Stories allow you to feature full screen video, photos, and audio, seamlessly.",
-      imageUrl:
-      "../../assets/pinkpanda.webp",    },
-  ];
-  export {featureSlides };
-  
+const featureSlides = [
+  {
+    title: "Complete control",
+    description:
+      "Every decision, from how you monetize your story to how long it stays online, is up to you.",
+    imageUrl: "../../assets/hero-phone-frame-v2.png",
+  },
+  {
+    title: "On Brand",
+    description:
+      "With the ability to command every pixel, each story can be designed to fit your brand.",
+    imageUrl: "../../assets/hero-phone-frame-v2.png",
+  },
+  {
+    title: "Immersive",
+    description:
+      "Web Stories allow you to feature full screen video, photos, and audio, seamlessly.",
+    imageUrl: "../../assets/hero-phone-frame-v2.png",
+  },
+]
+export { featureSlides }
